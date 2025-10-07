5d3684ef4d8070421c5f74d4bc2555f57091fc02
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2025-10-08 00:35:12 +0300

    chore: setup monorepo with Turborepo

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { nestJsConfig } from "@library/eslint-config/nest-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default nestJsConfig;
---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,39 @@
+import { baseConfig } from "./base.js";
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/**
+ * A custom ESLint configuration for libraries that use Nest.js.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const nestJsConfig = [
+  ...baseConfig,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn'
+    },
+  },
+];
